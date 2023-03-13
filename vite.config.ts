a300b23e68205b6f79d61332eef31334df0c30dd
byeongdolee <bdbdbdlee@gmail.com>
2023-03-13 22:16:51 +0900

    vite 설정

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,13 @@ import { defineConfig } from 'vite';
 export default defineConfig({
 	plugins: [sveltekit()],
 	optimizeDeps: {
-		exclude: ["codemirror", "@codemirror/view", "@codemirror/lang-markdown",
-		"@codemirror/lang-java", "@codemirror/state", "@codemirror/theme-one-dark", "@codemirror/highlight", "@codemirror/matchbrackets", "@codemirror/basic-setup"],
+		exclude: [
+			'codemirror',
+			'@codemirror/view',
+			'@codemirror/lang-java',
+			'@codemirror/state',
+			'@codemirror/theme-one-dark',
+			'@codemirror/basic-setup'
+		]
 	}
 });
